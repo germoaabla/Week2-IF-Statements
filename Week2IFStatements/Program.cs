e1f65e2f8f244a737220f7e01a5999102c436218
Germo Aabla <germo.aabla.it@gmail.com>
2024-09-15 17:57:56 +0300

    week 2

---
+++ Week2IFStatements/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.WriteLine("Enter your lastname:");
+string lastname = Console.ReadLine();
+
+Console.WriteLine("Enter your gender (f/m/n):");
+char userGender = char.Parse(Console.ReadLine());
+
+string greeting;
+
+if (userGender == 'f')
+{
+    greeting = "Ms.";
+}
+else if (userGender == 'm')
+{
+    greeting = "Mr.";
+}
+else
+{
+    greeting = "";
+}
+
+
+Console.WriteLine($"Hello, {greeting} {lastname}!");
+
+Console.WriteLine("Enter your year of birth:");
+
+int usersYOB = Int32.Parse(Console.ReadLine());
+
+int currentYear = DateTime.Now.Year;
+
+//Console.WriteLine($"Current year: {currentYear}");
+
+int userAge = currentYear - usersYOB;
+
+//int userAge = Int32.Parse(Console.ReadLine());
+
+if (userAge >= 13)
+{
+    Console.WriteLine($"You are {userAge} - old enough to have an account");
+}
+else
+{
+    Console.WriteLine($"You are {userAge} - too young to have an account");
+}
